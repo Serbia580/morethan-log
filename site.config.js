e55c8907abfdca185f3700ef2a2cd9de9b08be4e
Serbia580 <145875645+Serbia580@users.noreply.github.com>
2023-11-03 04:49:53 +0100

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -26,7 +26,7 @@ const CONFIG = {
   link: "https://morethan-log.vercel.app",
   since: 2022, // If leave this empty, current year will be used.
   lang: "zh-CN, // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
-  ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
+  ogImageGenerateURL: "https://ouzhou8.eu.org", // The link to generate OG image, don't end with a slash
 
   // notion configuration (required)
   notionConfig: {
